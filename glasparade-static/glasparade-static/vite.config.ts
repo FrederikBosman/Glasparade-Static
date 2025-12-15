5f9669130c2389579bd4480a8af200c7349a8ecc
Frederik-Partner <Frederik@partner-us.co.za>
2025-12-15 11:02:32 +0200

    updated vite config base to allow custom domain

--- glasparade-static/glasparade-static/vite.config.ts
+++ glasparade-static/glasparade-static/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/Glasparade-Static/",
+    base: "/",
     plugins: [
         tailwindcss(),
         react()
