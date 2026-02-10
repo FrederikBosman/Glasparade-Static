fda275d50882ef8c47cc39a605f85bc3ee3456c6
George Frederik Bosman <frederik.bosman09@gmail.com>
2026-02-10 20:40:23 +0200

    moved icons to an icon folder

--- glasparade-static/glasparade-static/src/components/Icons.tsx
+++ glasparade-static/glasparade-static/src/components/Icons.tsx
@@ -3,7 +3,7 @@ import type { SVGProps } from "react";
 export function IconSpotify(props: SVGProps<SVGSVGElement>) {
     return (
         <img
-            src="/spotify.png"
+            src="/Icons/spotify.png"
             alt="Spotify"
             className="h-7 w-7"
         />
@@ -13,7 +13,7 @@ export function IconSpotify(props: SVGProps<SVGSVGElement>) {
 export function IconInstagram(props: React.SVGProps<SVGSVGElement>) {
     return (
         <img
-            src="/instagram.png"
+            src="/Icons/instagram.png"
             alt="Instagram"
             className="h-7 w-7"
         />
@@ -22,7 +22,7 @@ export function IconInstagram(props: React.SVGProps<SVGSVGElement>) {
 export function IconTikTok(props: React.SVGProps<SVGSVGElement>) {
     return (
         <img
-            src="/tik-tok.png"
+            src="/Icons/tik-tok.png"
             alt="TikTok"
             className="h-7 w-7"
         />
@@ -31,7 +31,7 @@ export function IconTikTok(props: React.SVGProps<SVGSVGElement>) {
 export function IconYouTube(props: SVGProps<SVGSVGElement>) {
     return (
         <img
-            src="/play.png"
+            src="/Icons/play.png"
             alt="Youtube Music"
             className="h-7 w-7"
         />
@@ -41,7 +41,7 @@ export function IconYouTube(props: SVGProps<SVGSVGElement>) {
 export function IconAppleMusic(props: SVGProps<SVGSVGElement>) {
     return (
         <img
-            src="/music.png"
+            src="/Icons/music.png"
             alt="Apple Music"
             className="h-7 w-7"
         />
